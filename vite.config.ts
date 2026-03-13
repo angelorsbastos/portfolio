726a45479bdac886cd71c487e9a0daa858143773
Ângelo Rodrigo Sousa Bastos <82399046+relito17@users.noreply.github.com>
2026-03-13 10:51:32 +0100

    config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // ADD YOUR BASE PATH HERE:
+  base: "/portofolio/", // <-- Change this if your GitHub repo name is different!
+
   server: {
     host: "::",
     port: 8080,
